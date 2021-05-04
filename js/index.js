1ba6da8036e19d7ef366ee222572b35287115140
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-05-04 15:30:49 -0600

    a few more event listeners

--- js/index.js
+++ js/index.js
@@ -13,10 +13,32 @@ logo.addEventListener('mouseover', function(e){
 // CLICK
 
 const nav = document.querySelectorAll('.nav-link');
-console.log(nav);
+
 
 Array.from(document.links).forEach(function(link){
     link.addEventListener('click', function(evn){
         link.style.color ='green';
     })
+});
+
+// AUXCLICK
+
+const footer = document.querySelector('.footer');
+
+footer.addEventListener('auxclick', function(e){
+    e.target.textContent = 'Who said you could do that?';
 })
+
+// DOUBLE CLICK
+
+const button = document.querySelector('.btn');
+
+button.addEventListener('dbclick', function(e){
+    button.style.color = '#17A2B8';
+});
+
+// KEYDOWN
+
+document.addEventListener('keydown', function(e){
+    e.target.style.backgroundColor= '#17A2B8';
+});
