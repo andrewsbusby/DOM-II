947c6a2501d856d8277f3fcbf0fb34254addceb7
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-05-04 14:52:17 -0600

    added a few event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+ 
+
+// MOUSE OVER
+
+const logo = document.querySelector('.logo-heading');
+
+
+logo.addEventListener('mouseover', function(e){
+    logo.style.color = '#D4673F';
+});
+
+// CLICK
+
+const nav = document.querySelectorAll('.nav-link');
+console.log(nav);
